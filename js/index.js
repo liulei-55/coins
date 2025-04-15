b4842881ea4d8913287b83cdc772346432d791b2
liulei-55 <ljsxgz_55@163.com>
2025-04-15 21:01:44 +0800

    add new version

--- js/index.js
+++ js/index.js
@@ -3,10 +3,15 @@ const loginForm = document.getElementById('loginForm');
 loginForm.addEventListener('submit', function (e) {
     e.preventDefault();
     const password = document.getElementById('password').value;
+    const version = document.querySelector('input[name="option"]:checked').value;
 
     if (password === '131419') {
-        window.location.href = './pages/getcoins.html';
+        if (version === 'version1' || version === null) {
+            window.location.href = './pages/getcoins.html';
+        } else {
+            window.location.href = './pages/getcoins2.html';
+        }
     } else {
         alert('Wrong password. Please try again.');
-        }
+    }
 });
